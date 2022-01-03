bb2e77a52ae63deae03b1d3175877a13c1c9cb57
Jay Welsh <thealchemicalopus@gmail.com>
2022-01-03 14:04:55 +0200

    fix(SeenConstants): revert constant -> immutable change

--- contracts/domain/SeenConstants.sol
+++ contracts/domain/SeenConstants.sol
@@ -14,11 +14,11 @@ contract SeenConstants {
     string internal constant ESCROW_TICKET_URI = "https://api.seen.haus/ticket/metadata/";
 
     // Access Control Roles
-    bytes32 internal immutable ADMIN = keccak256("ADMIN");                   // Deployer and any other admins as needed
-    bytes32 internal immutable SELLER = keccak256("SELLER");                 // Approved sellers amd Seen.Haus reps
-    bytes32 internal immutable MINTER = keccak256("MINTER");                 // Approved artists and Seen.Haus reps
-    bytes32 internal immutable ESCROW_AGENT = keccak256("ESCROW_AGENT");     // Seen.Haus Physical Item Escrow Agent
-    bytes32 internal immutable MARKET_HANDLER = keccak256("MARKET_HANDLER"); // Market Handler contracts
-    bytes32 internal immutable UPGRADER = keccak256("UPGRADER");             // Performs contract upgrades
+    bytes32 internal constant ADMIN = keccak256("ADMIN");                   // Deployer and any other admins as needed
+    bytes32 internal constant SELLER = keccak256("SELLER");                 // Approved sellers amd Seen.Haus reps
+    bytes32 internal constant MINTER = keccak256("MINTER");                 // Approved artists and Seen.Haus reps
+    bytes32 internal constant ESCROW_AGENT = keccak256("ESCROW_AGENT");     // Seen.Haus Physical Item Escrow Agent
+    bytes32 internal constant MARKET_HANDLER = keccak256("MARKET_HANDLER"); // Market Handler contracts
+    bytes32 internal constant UPGRADER = keccak256("UPGRADER");             // Performs contract upgrades
 
 }
